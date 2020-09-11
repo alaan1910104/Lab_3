61d13f614bd4b471b79c388783808195e070a975
Andre <alaan1910104@etu.cegepjonquiere.ca>
2020-09-11 11:55:20 -0400

    commit 2, manque: pretty et fade

--- src/ClickHandler.java
+++ src/ClickHandler.java
@@ -1,60 +1,84 @@
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.scene.Group;
 import javafx.scene.control.Label;
+import javafx.scene.control.Labeled;
 import javafx.util.Duration;
+
+import java.util.List;
 
 public class ClickHandler {
     private long clicks;
     private int newclik;
     private int clickAdd;
     private int clickCombo;
-    private Label score;
-    private Timeline tm;
-    private int duration1;
-    private int duration2;
+    private List<Labeled> lbld;
+    private Timeline tm1;
+    private Timeline tm2;
+    private double duration1;
+    private int clickssec;
+    private int[] costs;
 
-    public ClickHandler(Label score){
+    public ClickHandler(List<Labeled> lbld){
         this.clicks = 0;
         this.newclik = 0;
         this.clickAdd = 0;
         this.clickCombo = 1;
-        this.score = score;
+        this.lbld = lbld;
         this.duration1 = 5;
-        this.duration2 = 5;
+        this.clickssec = 0;
 
-        KeyFrame kf = new KeyFrame(Duration.seconds(this.duration1), event -> {add(); updateText(true);});
-        this.tm = new Timeline();
-        tm.setCycleCount(Timeline.INDEFINITE);
-        tm.setAutoReverse(true);
-        tm.getKeyFrames().add(kf);
-        tm.setOnFinished((event) -> {System.out.print("done!");});
+        this.costs = new int[]{10, 20, 30, 40, 50};
+
+        this.tm1 = new Timeline();
+        tm1.setCycleCount(Timeline.INDEFINITE);
+        tm1.setAutoReverse(true);
+        this.tm2 = new Timeline();
+        tm2.setCycleCount(Timeline.INDEFINITE);
+        tm2.setAutoReverse(true);
     }
 
     public void addClickAuto(){
-        if(this.duration1 == 5){
-            this.tm.play();
-            this.duration1 = this.duration1 / 2;
-        }
-        else {
-            this.duration1 = this.duration1 / 2;
-        }
+
+        this.tm1.getKeyFrames().add(
+                new KeyFrame(Duration.seconds(this.duration1), event -> {add();}));
+
+        this.duration1 = this.duration1 / 2;
+        this.tm1.play();
+    }
+
+    public void addClicksec(){
+        this.tm2.getKeyFrames().add(
+                new KeyFrame(Duration.seconds(1), event -> { this.clicks = this.clicks + this.clickssec; updateText(true);})
+        );
+        clickssec++;
+        this.tm2.play();
     }
 
     public void add(){
         setNewclik();
         this.clicks = this.clicks + this.newclik;
+        updateText(true);
     }
 
     public void updateText(boolean possible){
         if(possible){
-            this.score.setText("Clickys Clicked: " + this.clicks);
+            this.lbld.get(0).setText("Clickys Clicked: " + this.clicks);
+            this.lbld.get(2).setText( "+1 Clicky / click! Total: " + getClickAdd() + "\n $: " + this.costs[0]);
+            this.lbld.get(3).setText( "+" + this.clickssec + " Clicky / sec!" + "\n $: " + this.costs[1]);
+            this.lbld.get(4).setText( "Click automatique /  " + this.duration1 + " sec!" + "\n $: " + this.costs[2]);
+            this.lbld.get(5).setText( "Clicky combo! Total: x" + getClickCombo() + "\n $: " + this.costs[3]);
+            this.lbld.get(6).setText( "ULTRACLICK" + "\n $: " + this.costs[4]);
         }
-        else{this.score.setText("Clicks Insoufisants!");}
+        else{
+            this.lbld.get(0).setText( "Clickys insouficents!");
+        }
     }
 
     public void buy(int cost){
         this.clicks = this.clicks - cost;
         if(this.clicks < 0){ this.clicks = 0;}
+        updateText(true);
     }
 
     public void ultraClick(){
@@ -72,10 +96,15 @@ public class ClickHandler {
 
     public void setNewclik() {
         this.newclik = (1 + this.clickAdd) * this.clickCombo;
+        this.lbld.get(1).setText( "  (+" + this.newclik + ")");
     }
 
     public int getClickAdd() {
         return clickAdd;
+    }
+
+    public int[] getCosts() {
+        return costs;
     }
 
     public void addClickAdd() {
--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,11 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Labeled;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main extends Application {
@@ -24,19 +26,33 @@ public class Main extends Application {
 
         // initialization
 
+        List<Labeled> lbld = new ArrayList<>();
+
+
+
 
         Label score= new Label();
-        ClickHandler ch = new ClickHandler(score);
-        ch.updateText(true);
-        Label tempscore = new Label("  (+" + ch.getNewclik() + ")");
+        lbld.add(score);
+        Label tempscore = new Label();
+        lbld.add(tempscore);
+
 
         Button btn = new Button("Clicky!");
+        Button upgrd1 = new Button();
+        lbld.add(upgrd1);
+        Button upgrd2 = new Button();
+        lbld.add(upgrd2);
+        Button upgrd3 = new Button();
+        lbld.add(upgrd3);
+        Button upgrd4 = new Button();
+        lbld.add(upgrd4);
+        Button upgrd5 = new Button();
+        lbld.add(upgrd5);
 
-        Button upgrd1 = new Button("+1 Clicky / click!");
-        Button upgrd2 = new Button("+1 Clicky / 5 sec!");
-        Button upgrd3 = new Button("Click automatique!");
-        Button upgrd4 = new Button("Clicky combo!");
-        Button upgrd5 = new Button("ULTRACLICK");
+
+        ClickHandler ch = new ClickHandler(lbld);
+
+        ch.updateText(true);
 
 
         Group root = new Group(btn, score, tempscore, upgrd1, upgrd2, upgrd3, upgrd4, upgrd5);
@@ -48,44 +64,45 @@ public class Main extends Application {
         score.setScaleY(1.5);
 
 
+        int dif = 25;
+        int height = 40;
+        int width = 120;
+
         //btn
-        btn.setMinSize(80, 145);
+        btn.setMinSize((width + 50), (5 * dif) + (5 * height));
         btn.setTranslateY(20);
         btn.setOnAction(event -> {
             ch.add();
-            score.setText("Clickys Clicked: " + ch.getClicks());
-            tempscore.setText("  (+" + ch.getNewclik() + ")");
         });
 
-
         // Upgrade 1
-        upgrd1.setMinSize(40,20);
-        upgrd1.setTranslateY(20);
-        upgrd1.setTranslateX(100);
+        upgrd1.setMinSize(width,height);
+        upgrd1.setTranslateY(dif);
+        upgrd1.setTranslateX(200);
         upgrd1.setOnAction(event -> { action(1, ch);});
 
         // Upgrade 2
-        upgrd2.setMinSize(40,20);
-        upgrd2.setTranslateY(50);
-        upgrd2.setTranslateX(100);
+        upgrd2.setMinSize(width,height);
+        upgrd2.setTranslateY( (2 * dif) + height);
+        upgrd2.setTranslateX(200);
         upgrd2.setOnAction(event -> { action(2, ch);});
 
         // Upgrade 3
-        upgrd3.setMinSize(40, 20);
-        upgrd3.setTranslateY(80);
-        upgrd3.setTranslateX(100);
+        upgrd3.setMinSize(width, height);
+        upgrd3.setTranslateY((3 * dif) + (2 * height));
+        upgrd3.setTranslateX(200);
         upgrd3.setOnAction(event -> { action(3, ch);});
 
         // Upgrade 4
-        upgrd4.setMinSize(40,20);
-        upgrd4.setTranslateY(110);
-        upgrd4.setTranslateX(100);
+        upgrd4.setMinSize(width, height);
+        upgrd4.setTranslateY((4 * dif) + (3 * height));
+        upgrd4.setTranslateX(200);
         upgrd4.setOnAction(event -> { action(4, ch);});
 
         // Upgrade 5
-        upgrd5.setMinSize(40,20);
-        upgrd5.setTranslateY(140);
-        upgrd5.setTranslateX(100);
+        upgrd5.setMinSize(width, height);
+        upgrd5.setTranslateY((5 * dif) + (4 * height));
+        upgrd5.setTranslateX(200);
         upgrd5.setOnAction(event -> { action(5, ch);});
 
         //Set and show
@@ -97,27 +114,13 @@ public class Main extends Application {
 
     public static void action(int btnumb, ClickHandler ch){
 
-        int cost = 0;
-
-        switch (btnumb){
-            case 1: cost = 10;
-            break;
-            case 2: cost = 20;
-            break;
-            case 3: cost = 30;
-            break;
-            case 4: cost = 40;
-            break;
-            default: cost = 50;
-        }
-
-        boolean reussi = ch.getClicks() >= cost;
+        boolean reussi = ch.getClicks() >= ch.getCosts()[btnumb - 1];
         if(reussi){
 
             switch(btnumb){
                 case 1: ch.addClickAdd();
                 break;
-                case 2: ;
+                case 2: ch.addClicksec();
                 break;
                 case 3: ch.addClickAuto();
                 break;
@@ -126,7 +129,8 @@ public class Main extends Application {
                 case 5: ch.ultraClick();
                 break;
             }
-            ch.buy(cost);
+            ch.buy(ch.getCosts()[btnumb - 1]);
+            ch.getCosts()[btnumb - 1] = ch.getCosts()[btnumb - 1] * 2;
         }
         ch.updateText(reussi);
     }
